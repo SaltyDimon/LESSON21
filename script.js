c14827a1a839429a835de4286f295813dc07679d
SaltyDimon <126015754+SaltyDimon@users.noreply.github.com>
2024-02-23 17:02:08 +0000

    Зробили кружечки

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ function draw(event) {
     let ctx = canvas.getContext("2d");
     let rect = canvas.getBoundingClientRect();
 
-    let posX = event.ClientX;
-    let posY = event.ClientY;
+    let posX = event.ClientX - rect.left;
+    let posY = event.ClientY - rect.top;
 
     ctx.fillStyle = "#00ff00"
 
+    ctx.beginPath();
+
     ctx.arc(posX, posY, 50, 0, 2 * Math.PI);
 
-    
+    ctx.fill()
+
+
 }
