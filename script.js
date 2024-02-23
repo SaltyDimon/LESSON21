9ac7cdb3b74a886a3035562987a2cf2aea7610ad
SaltyDimon <126015754+SaltyDimon@users.noreply.github.com>
2024-02-23 16:33:18 +0000

    Додати малювання кола по клікові мишки

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function draw(event) {
     let canvas = document.getElementById(imgCanvas);
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.ClientX;
+    let posY = event.ClientY;
+
+    ctx.fillStyle = "#00ff00"
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI);
+
+    
 }
